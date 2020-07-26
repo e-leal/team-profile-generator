5ae7d6ec84fae0df099c8635f3be42b9f5e53ac3
e-leal <leal.elyssa03@gmail.com>
2020-07-26 17:00:32 -0500

    installed jest and added Employee class and test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require('../lib/Employee');
+
+console.log(new Employee());
+
+test('Creates an employee object', () => {
+    const employee = new Employee('Dave', 1, 'dbomb@email.com');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('@'));
+});
